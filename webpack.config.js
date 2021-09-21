5fd329f7758fcb310cc6c6cc586630ecb1d171ff
Luis <lfmnovaes@gmail.com>
2021-09-21 19:10:12 -0300

    Add index.html and style.css templates

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
