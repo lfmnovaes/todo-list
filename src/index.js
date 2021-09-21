d13e50d2d9f372650693e8df930a8298cf1974c5
Luis <lfmnovaes@gmail.com>
2021-09-21 18:48:55 -0300

    after asset management

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack4'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
