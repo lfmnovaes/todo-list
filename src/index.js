b2acb8326dbf35852d90da14112bf882c94841c8
Luis <lfmnovaes@gmail.com>
2021-09-22 17:18:33 -0300

    Add styling
    * Add style for every aspect of the To Do List
    * Add fontawesome through webpack

--- src/index.js
+++ src/index.js
@@ -1,28 +1,60 @@
 import taskList from './tasks.js';
 import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+dom.watch();
+
+const boxList = document.getElementById('box-list');
 
 let populateList = () => {
+  const list = document.createElement('ul');
+  list.classList.add('item-list');
   taskList.forEach((task) => {
-    console.log(task);
+    const item = document.createElement('li');
+    const cBox = document.createElement('input');
+    cBox.type = 'checkbox';
+    const label = document.createElement('label');
+    label.innerText = task.description;
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-ellipsis-v');
+
+    item.appendChild(cBox);
+    item.appendChild(label);
+    item.appendChild(icon);
+    list.appendChild(item);
   });
+  return list;
 }
 
 window.onload = () => {
-  const list = document.getElementById('list');
-  const item = document.createElement('li');
-  const label = document.createElement('p');
-  //const update = document.createElement('button');
+  const top = document.createElement('div');
+  const title = document.createElement('h2');
+  const refresh = document.createElement('i');
 
-  item.classList.add('item');
-  label.innerHTML = "Today's To Do";
-  list.appendChild(label);
-  list.appendChild(item);
-
+  top.classList.add('top');
+  title.innerHTML = "Today's To Do";
+  refresh.classList.add('fas', 'fa-sync-alt');
+  top.appendChild(title);
+  top.appendChild(refresh);
+  boxList.appendChild(top);
   const form = document.createElement('form');
   const input = document.createElement('input');
+  const arrow = document.createElement('i');
+  input.type = 'text';
   input.placeholder = 'Add to your list ...';
+  arrow.classList.add('fas', 'fa-level-down-alt');
   form.appendChild(input);
-  list.appendChild(form);
+  form.appendChild(arrow);
+  boxList.appendChild(form);
 
-  populateList();
+  boxList.appendChild(populateList());
+
+  const clearBtn = document.createElement('button');
+  clearBtn.classList.add('clear-btn');
+  clearBtn.innerText = 'Clear all completed';
+  boxList.appendChild(clearBtn);
 };
