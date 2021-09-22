8e2fd4f79b6cf391c56c5d02196a5c823d01e662
Luis <lfmnovaes@gmail.com>
2021-09-22 12:49:29 -0300

    Add initial box for the list
    * Remove unused stuff from index.js
    * Add initial style for the box list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-//import _ from 'lodash';
 import taskList from './tasks.js';
 import './style.css';
 
-function component() {
-  const tasks = document.getElementById('list');
-  const task = document.createElement('li');
-  const btn = document.createElement('button');
-
-  //task.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  task.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = console.log(taskList);
-
-  task.appendChild(btn);
-
-  return task;
+let populateList = () => {
+  taskList.forEach((task) => {
+    console.log(task);
+  });
 }
 
-document.body.appendChild(component());
+window.onload = () => {
+  const list = document.getElementById('list');
+  const item = document.createElement('li');
+  const label = document.createElement('p');
+  //const update = document.createElement('button');
+
+  item.classList.add('item');
+  label.innerHTML = "Today's To Do";
+  list.appendChild(label);
+  list.appendChild(item);
+
+  const form = document.createElement('form');
+  const input = document.createElement('input');
+  input.placeholder = 'Add to your list ...';
+  form.appendChild(input);
+  list.appendChild(form);
+
+  populateList();
+};
